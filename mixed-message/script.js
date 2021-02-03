ef9e98f681f5d15c6e40b3565f9dcbd61729798d
Shiwei Yu <shiwei1991@gmail.com>
2021-02-03 16:52:18 +0100

    fix source for mixed-message v3

--- mixed-message/script.js
+++ mixed-message/script.js
@@ -13,71 +13,33 @@ app.appendChild(h1);
 app.appendChild(logo);
 app.appendChild(container);
 
-// example with fetch API
-fetch('https://quotes.legionnaire.world/') // change to http://localhost:5000 to run locally
-  .then(
-    (response) => {
-      return response.json();
-    },
-    (networkError) => {
-      const errorMessage = document.createElement('marquee');
-      errorMessage.textContent = `Ouupooos, it's not working!`;
-      app.appendChild(errorMessage);
-    }
-  )
-  .then((data) => {
-    // Work with JSON data here
-    // console.log(data);
-    data.forEach((quotes) => {
+const fetchQuote = async () => {
+  try {
+    const req = await fetch('https://quote.numaga.fr/api/quotes');
+    const jsonReq = await req.json();
+    const data = await jsonReq.qutoes.content;
+    data.forEach((quote) => {
+      const { quote, author, tag } = quote;
       const card = document.createElement('div');
       card.setAttribute('class', 'card');
 
       const h1 = document.createElement('h1');
-      h1.textContent = quotes.GENRE.toUpperCase();
+      h1.textContent = tag.toUpperCase();
 
       const p = document.createElement('p');
       //   quotes.QUOTE = quotes.QUOTE.substring(0, 300);
-      p.textContent = `" ${quotes.QUOTE} " by ${quotes.AUTHOR}`;
+      p.textContent = `" ${quote} " by ${author}`;
 
       container.appendChild(card);
       card.appendChild(h1);
       card.appendChild(p);
     });
-  });
-// .catch((err) => {
-//   const errorMessage = document.createElement('marquee');
-//   errorMessage.textContent = `Ouupooos, it's not working!`;
-//   app.appendChild(errorMessage);
-// });
+  } catch (err) {
+    const errorMessage = document.createElement('marquee');
+    errorMessage.textContent = `Ouupooos, it's not working!`;
+    app.appendChild(errorMessage);
+    console.error(err);
+  }
+};
 
-// example with XHR
-
-// const request = new XMLHttpRequest();
-// request.open('GET', 'http://localhost:8000/', true);
-// request.onload = function () {
-//   // Begin accessing JSON data here
-//   const data = JSON.parse(this.response);
-//   if (request.status >= 200 && request.status < 400) {
-//     data.forEach((quotes) => {
-//       const card = document.createElement('div');
-//       card.setAttribute('class', 'card');
-
-//       const h1 = document.createElement('h1');
-//       h1.textContent = quotes.GENRE.toUpperCase();
-
-//       const p = document.createElement('p');
-//       //   quotes.QUOTE = quotes.QUOTE.substring(0, 300);
-//       p.textContent = `"${quotes.QUOTE}>" by ${quotes.AUTHOR}`;
-
-//       container.appendChild(card);
-//       card.appendChild(h1);
-//       card.appendChild(p);
-//     });
-//   } else {
-//     const errorMessage = document.createElement('marquee');
-//     errorMessage.textContent = `Gah, it's not working!`;
-//     app.appendChild(errorMessage);
-//   }
-// };
-
-// request.send();
+window.addEventListener('load', fetchQuote);
